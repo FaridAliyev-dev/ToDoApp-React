9b98bac4be0580d32ba9a0bd6a2b3679540cffe1
FaridAliyev-dev <farid.aliyevv258@gmail.com>
2025-02-07 09:20:38 +0400

    feat: add new module exports and update package dependencies

--- toDoApp-React/vite.config.js
+++ toDoApp-React/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: "./", // Ensures Vite finds index.html in root
+  build: {
+    outDir: "dist", // Ensure build output goes to 'dist'
+  },
+});
